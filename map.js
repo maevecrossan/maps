e088f954f489a9a079df75176003d72667ee1968
Maeve Crossan <maevecrossan@gmail.com>
2024-09-15 15:30:00 +0000

    Add challenge code

--- map.js
+++ map.js
@@ -83,3 +83,38 @@ const addAges = students.map( students => {
   };
 });
 console.log(addAges);
+
+// maps challenge
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, maths: 77, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+
+let [john, ...rest] = students.map( students => [students.name, students.results]);
+
+console.log(john);
+console.log(rest);
